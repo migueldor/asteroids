7803592ab169045e39167726eff0dcddf34355cc
migueldor <migueldor.balta@gmail.com>
2025-11-19 11:39:57 -0600

    added framerate and circleshape

--- main.py
+++ main.py
@@ -2,27 +2,57 @@ import pygame
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
-    while True == True:
+    while True:
         log_state()
 
-        #for event in pygame.event.get():
-        #    if event.type == pygame.QUIT:
-        #        return
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
 
         screen.fill("black")
         pygame.display.flip()
 
-      
-    
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+'''
 def main():
     print("Starting Asteroids with pygame version: " + pygame.version.ver)
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        log_state()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black")
+        pygame.display.flip()
+
+        clock.tick(60)
+        dt = clock.tick(60)/1000
+''' 
+'''    
+def main():
+    print("Starting Asteroids with pygame version: " + pygame.version.ver)
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+'''
 
 if __name__ == "__main__":
     main()
