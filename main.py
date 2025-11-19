dfc38faa5f8f59ef1b9e56edee073baa05785d7e
migueldor <migueldor.balta@gmail.com>
2025-11-19 12:15:09 -0600

    now draws the player and can be rotated

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import pygame
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
@@ -17,8 +18,10 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
-
+        
         # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 '''
